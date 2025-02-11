c0d2f3bf379049303a4bc03ccdc94e604feff68c
fabri <fabricioflores515@gmail.com>
2025-02-10 21:30:27 -0400

    cambiando el plugin de alpha por dashboard nvim y ajustando el format on save

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		-- Estoy instalando el plugin 'dashboard-nvim' para crear un dashboard personalizado en Neovim.
+		"nvimdev/dashboard-nvim",
+
+		-- Le digo a Neovim que cargue este plugin cuando se inicie (evento 'VimEnter').
+		event = "VimEnter",
+
+		-- Configuro el plugin después de que se cargue.
+		config = function()
+			require("dashboard").setup({
+				-- Aquí puedo agregar configuraciones personalizadas para el dashboard.
+			})
+		end,
+
+		-- Le indico que este plugin necesita 'nvim-web-devicons' para mostrar íconos.
+		dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	},
+}
